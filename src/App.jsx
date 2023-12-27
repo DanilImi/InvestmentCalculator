257b179c98ce367e466aa81c387a6aaae318968e
Danil Razumov <dumirov03@mail.ru>
2023-12-27 14:16:12 +0300

    Fix: duration

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     expectedReturn: 6,
     duration: 10,
   });
-
+  const inputIsValid = userInput.duration >= 1;
   const handleChange = (inputIdentifer, newValue) => {
     setUserInput((prevUserInput) => {
       return {
@@ -19,11 +19,15 @@ function App() {
       };
     });
   };
+
   return (
     <>
       <Header />
       <UserInput onChange={handleChange} userInput={userInput} />
-      <Results input={userInput} />
+      {!inputIsValid && (
+        <p className="center">Please enter a duration greater than 0</p>
+      )}
+      {inputIsValid && <Results input={userInput} />}
     </>
   );
 }
