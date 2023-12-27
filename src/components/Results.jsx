34211734ceba7eb71843a4d012246f7ef65e2125
Danil Razumov <dumirov03@mail.ru>
2023-12-27 14:07:59 +0300

    Feat: add table result, calc value for table

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,7 +1,40 @@
-import { calculateInvestmentResults } from "../util/investment";
+import { calculateInvestmentResults, formatter } from "../util/investment";
 
 export const Results = ({ input }) => {
   const resultsData = calculateInvestmentResults(input);
-  console.log("Results  resultsData:", resultsData);
-  return <div>Results...</div>;
+  const initialInvestment =
+    resultsData[0].valueEndOfYear -
+    resultsData[0].interest -
+    resultsData[0].annualInvestment;
+  return (
+    <table id="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        {resultsData.map(
+          ({ year, interest, valueEndOfYear, annualInvestment }) => {
+            const totalInterest =
+              valueEndOfYear - annualInvestment * year - initialInvestment;
+            const totalAmountInvested = valueEndOfYear - totalInterest;
+            return (
+              <tr key={year}>
+                <td>{year}</td>
+                <td>{formatter.format(valueEndOfYear)}</td>
+                <td>{formatter.format(interest)}</td>
+                <td>{formatter.format(totalInterest)}</td>
+                <td>{formatter.format(totalAmountInvested)}</td>
+              </tr>
+            );
+          }
+        )}
+      </tbody>
+    </table>
+  );
 };
